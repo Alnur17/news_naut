9ab860df450771fb2034f958d90d57d728226609
Alnur17 <98782302+Alnur17@users.noreply.github.com>
2024-05-23 13:01:07 +0600

    style
    Style file addedd

---
+++ lib/constant/style.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import 'color_palate.dart';
+
+const styleBB24 = TextStyle(
+  //color: Color(0xffFFA500),
+  color: textBlue,
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+
+const styleBB20 = TextStyle(
+  //color: Color(0xffFFA500),
+  color: textBlue,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+const styleBB16 = TextStyle(
+  //color: Color(0xffFFA500),
+  color: textBlue,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+const styleGB24 = TextStyle(
+  color: textGold,
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+const styleGB20 = TextStyle(
+  color: textGold,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+const styleGB16 = TextStyle(
+  color: textGold,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+
+const styleWB24 = TextStyle(
+  color: justWhite,
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+);
+const styleWB20 = TextStyle(
+  color: justWhite,
+  fontSize: 20,
+  fontWeight: FontWeight.bold,
+);
+const styleWB18 = TextStyle(
+  color: justWhite,
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+);
+const styleWB16 = TextStyle(
+  color: justWhite,
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+);
+const styleWB14 = TextStyle(
+  color: justWhite,
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+);
+const styleWB12 = TextStyle(
+  color: justWhite,
+  fontSize: 12,
+  fontWeight: FontWeight.bold,
+);
